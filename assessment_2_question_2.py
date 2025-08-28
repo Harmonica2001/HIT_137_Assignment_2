79bac42da7ef5fd066c3caecfbd824713032ad84
Haroon2001 <63156452+Haroon2001@users.noreply.github.com>
2025-08-28 20:56:45 +0930

    I changed the name for the assessment_2_question_2 file

--- assessment_2_question_2.py
+++ assessment_2_question_2.py
@@ -5,35 +5,26 @@ Md Adnan Abir = s382198
 Simbarashe Mutyambizi = s385833
 Najmus Sakeeb= s393942
 """
-
-
 #%% 
 # Declaration of libraries
 
 import pandas as pd 
-import numpy as np
 import glob
 import os
-import statistics as st
-
 #Question 2: Seasonal Average
 # Create empty files for outputs of all 3 parts of Question 2
 # If the files already exist, they will be overwritten with empty content
 file_name = "average_temp.txt"
 content = ""
 with open(file_name, "w") as file:
-    file.write(content)
-   
+    file.write(content)  
 file_name_2 = "largest_temp_range_station.txt"
 with open(file_name_2, "w") as file:
     file.write(content)
-       
 file_name_3 = "temperature_stability_stations.txt"
 with open(file_name_3, "w") as file:
     file.write(content)
-
-# reads the csv file
-df = pd.read_csv('temperatures\stations_group_1986.csv')
+# this function will read and concat all of them into one big dataframe
 def read_and_concat_csv(folder_path):
     # Get all CSV files in the folder
     csv_files = glob.glob(os.path.join(folder_path, "*.csv"))
@@ -48,7 +39,6 @@ def read_and_concat_csv(folder_path):
     big_df = pd.concat(dfs, ignore_index=True)
     
     return big_df
-
 # Here I utilize the function and we can get a concatenated dataframe of all the csv files in the temperatures folder
 folder_path = r"D:\Masters\Masters_work\Study\Y1_S1\HIT137\Assignment_2\HIT_137_Assignment_2\temperatures"
 df = read_and_concat_csv(folder_path)
@@ -59,18 +49,15 @@ summer_cols = ['December', 'January', 'February']
 autumn_cols = ['March', 'April', 'May']
 winter_cols = ['June', 'July', 'August']
 spring_cols = ['September', 'October', 'November']
-
 # Calculate seasonal averages
 summer_avg = df_months[summer_cols].mean(axis=1).mean().round(1)
 autumn_avg = df_months[autumn_cols].mean(axis=1).mean().round(1)
 winter_avg = df_months[winter_cols].mean(axis=1).mean().round(1)
 spring_avg = df_months[spring_cols].mean(axis=1).mean().round(1)
-
 # Create new DataFrame to contain the seasonal averages
 df_season_avg = pd.DataFrame([
     [summer_avg, autumn_avg, winter_avg, spring_avg]  # second row values
 ], columns=['Summer', 'Autumn', 'Winter', 'Spring'])
-
 # generate the output text that is going to be written to the average_temp.txt file
 output_text = f"""
 Summer: {summer_avg}°C
@@ -81,14 +68,10 @@ Spring: {spring_avg}°C
 #write file into the average_temp.txt file
 with open(file_name, "w") as f:
     f.write(output_text)
-
 # Question 2: Temparature Range 
-
 # group by the first column so all of the stations are grouped together
 df_sorted = df.sort_values(by=df.columns[0], key=lambda col: col.astype(str))
 df_sorted = df_sorted.reset_index(drop=True)
-
-
 # this function calculates the range of the temperatures for each station
 def calculate_range(df_sorted):
     # Define the number of columns to consider for max, min, and range calculations
@@ -124,7 +107,6 @@ def calculate_range(df_sorted):
     })
     max_col = 'Max Values'
     min_col = 'Min Values'
-
     # Calculate max and min values for each list
     df_new['Max'] = df_new[max_col].apply(lambda x: max(x))
     df_new['Min'] = df_new[min_col].apply(lambda x: min(x))
@@ -137,21 +119,16 @@ def calculate_range(df_sorted):
 # calcualte the range of the temperatures for each station and store it in the df_new dataframe    
 df_new = calculate_range(df_sorted)
 results = df_new.loc[[df_new['Highest_Range'].idxmax()]].reset_index(drop=True, inplace=False)
-
-
 # finding the row with the higheest range its corresponding max and min values
 Max_range = results["Highest_Range"].values[0]
 Max_station = results['Station_Name'].values[0]      
 Max_value = results["Max"].values[0]
 Min_value = results["Min"].values[0]
-
 # generate the output text that is going to be written to the largest_temp_range_station.txt file
 output_text_2 = f"""
 Station {Max_station}: Range {Max_range:.1f}°C(Max: {Max_value:.1f}°C, Min: {Min_value:.1f}°C)"""
 with open(file_name_2, "w") as f:
     f.write(output_text_2)
-
-
 # Question 2: Temperature Stability
 
 # generate the funciton that calculates the standard deviation of the temperatures for each station
@@ -181,21 +158,17 @@ def calculate_SD(df_sorted):
     df_new = pd.DataFrame({
         'Station_Name': first_values,
         'SD Values': SD_values,
-    })
-    
-    
+    }) 
     return df_new
 df_new = calculate_SD(df_sorted)
-
-
 # This will find he lasrgest and smallest standard deviation values and their corresponding station names
 SD_max_results = df_new.loc[[df_new['SD Values'].idxmax()]].reset_index(drop=True, inplace=False)
 SD_min_results = df_new.loc[[df_new['SD Values'].idxmin()]].reset_index(drop=True, inplace=False)
 # finding the row with the higheest range its corresponding max and min values
-MostStable_SD_Station = SD_max_results["Station_Name"].values[0]
-MostVariable_SD_Station = SD_min_results["Station_Name"].values[0]
-MostStable_SD_Value = SD_max_results["SD Values"].values[0]
-MostVariable_SD_Value = SD_min_results["SD Values"].values[0]
+MostStable_SD_Station = SD_min_results["Station_Name"].values[0]
+MostVariable_SD_Station = SD_max_results["Station_Name"].values[0]
+MostStable_SD_Value = SD_min_results["SD Values"].values[0]
+MostVariable_SD_Value = SD_max_results["SD Values"].values[0]
 # the output text that is going to be written to the temperature_stability_stations.txt file
 output_text_3 = f"""
 Most Stable: Station {MostStable_SD_Station}: StdDev {MostStable_SD_Value:.1f}°C
